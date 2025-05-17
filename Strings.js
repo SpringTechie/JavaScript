5d8eef740dde16cc1e91648ff48c1b39e383d386
springtechie <springframeworkdeveloper@gmail.com>
2025-05-17 21:08:31 +0530

    Adjusting and adding String notes.

--- Strings.js
+++ Strings.js
@@ -1,6 +1,54 @@
-x = "arun"
-y = "kumar";
-text =`Wlecome ${x} ${y}`;
-console.log(text)
+
+//x = `Hello ${y}`
+// z = new String("Arun")
+// k = new String("Arun")
+// console.log(z==k) // comparing two javacript objects returns false
+// console.log(z===k) // comparing two javacript objects returns false
+// console.log(typeof z)
+// console.log(x)
+// some imp String methods
+x = "  Arun how are  "
+console.log(x.length) // finds the length of String
+console.log(x.charAt(-1)) // find the char at particular position and  if specifed index in not found then returns nothing.
+console.log(x[6]) // to access the char 
+console.log(x.at(-3)) 
+// it is intro in ES6 if specifed index in not found then returns undefined
+// this method also accepts the negative index.and negative index starts from end pof the String.
+
+// To extract any part of String we have to use 
+// 1.substring(startIndex,endIndex) // specify start index and end index(Optional)
+// 2.slice(startIndex,endIndex)
+// startindex always should be small and end index always should be high.
+console.log(x.substring(0)) // from 0 index to last index(length-1)
+console.log(x.substring(1,2)) // from 2 index to end-index(excluded)
+console.log(x.slice(-12,-9)) // this methods works same as substring
+
+console.log(x.toUpperCase())
+console.log(x.toLowerCase())
+y = " you"
+
+
+console.log(x.concat(y)) 
+console.log(x.trim()) // trim will remove extra white spaces in a String.
+console.log(x.trimStart())
+console.log(x.trimEnd())
+
+console.log(y.repeat(2)) // repeats the String.
+
+console.log(x.replace("are","is")) // replace "are" with "is" and it only replaces firstMatch.
+console.log(x.replaceAll("are","is"))
+console.log(x.split(" ")) // splitting with white spaces and retuns an array.
+console.log(x.split(","))
+console.log(x.split("-"))
+
+console.log(x.indexOf('x')) // if specified char is present inside a String returns the index,if not returns the -1.
+console.log(x.lastIndexOf('r'))
+
+
+
+
+
+
+
 
 
--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-// by using literal
+// // by using literal
 let person =  {
     id : 1,
     lastname: "kumar",
@@ -13,54 +13,54 @@ let person =  {
        return this.firstname + this.lastname;
     }
     
-}
-// how to access the property of object.
-console.log(person.firstname)
-console.log(person["age"])
-// How to add new property to existing object.
-person.nation = "india";
-console.log(person)
-// nested object: An object contains of another object
-// how to access the nested object
-console.log(person.Car.brand)
-console.log(person.fullName())
+ }
+// // how to access the property of object.
+// console.log(person.firstname)
+// console.log(person["age"])
+// // How to add new property to existing object.
+// person.nation = "india";
+// console.log(person)
+// // nested object: An object contains of another object
+// // how to access the nested object
+// console.log(person.Car.brand)
+// console.log(person.fullName())
 
-// This by using new keyword
-const animal = new Object();
-animal.name = "Lion"
-animal.age = 34;
-console.log(animal)
-// to delete any property from object
-delete animal.name;
-console.log(animal)
-delete animal["name"]
+// // This by using new keyword
+// const animal = new Object();
+// animal.name = "Lion"
+// animal.age = 34;
+// console.log(animal)
+// // to delete any property from object
+// delete animal.name;
+// console.log(animal)
+// delete animal["name"]
 
-// to display person object in UI.
-document.getElementById("demo").innerHTML = person.fullName();
+// // to display person object in UI.
+// document.getElementById("demo").innerHTML = person.fullName();
 
 
-// 3rd way to create object in js. by using Constructor
-function Person(id,name,age) {
-    this.id = id;
-    this.name = name;
-    this.age = age;
-}
+// // 3rd way to create object in js. by using Constructor
+// function Person(id,name,age) {
+//     this.id = id;
+//     this.name = name;
+//     this.age = age;
+// }
 
-// the above is blue print to create a person object.
+// // the above is blue print to create a person object.
 
-// // create objects for above
-const person1 = new Person(1,"arun",23);
-const person2 = new Person(2,"pavan",34);
-console.log(person1)
+// // // create objects for above
+// const person1 = new Person(1,"arun",23);
+// const person2 = new Person(2,"pavan",34);
+// console.log(person1)
 
-person1.nation= "india" // adding to new property to the person1 object.
+// person1.nation= "india" // adding to new property to the person1 object.
 
-console.log(person1)
+// console.log(person1)
 
-// we want to add new proptery to constructor?
-Person.prototype.country = "USA"
-console.log(person1.country)
-console.log(person2.nation)
+// // we want to add new proptery to constructor?
+// Person.prototype.country = "USA"
+// console.log(person1.country)
+// console.log(person2.nation)
 
 // let person =  {
 //     id : 1,
@@ -103,3 +103,10 @@ console.log(person2.nation)
 
 
 
+let x = 0.2
+let y = 0.3
+console.log(x+y)
+
+console.log(typeof NaN)
+
+Infinity
