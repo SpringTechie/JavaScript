f6c84978c63841fcedf41ac567f26ba25957d3ed
springtechie <springframeworkdeveloper@gmail.com>
2025-05-24 21:27:40 +0530

    added array to repo

---
+++ Numbers.js
@@ -0,0 +1,39 @@
+let n1 = "16" // integer
+let n2 = 8 // float or double or decimal
+let n3 = 4
+console.log(n2/n1) // 8/"2" = 8/2
+console.log(n1/n2) // "16"/8 = 16/8
+
+console.log(n2*n1) // string converted into number
+console.log(n2-n1) // string converted into number
+let n4 = "arun"
+console.log(n4*n2) // NaN = Not a number when we try to divide or multiply or substract two diff data types it will return NaN.
+console.log(isNaN(n4*n2)) // NaN
+
+let n5 = 45
+console.log(typeof n5)
+
+let n6 = n5.toString()
+console.log(typeof n6)
+
+let n7 = "5"
+console.log(typeof n7)
+let n8 = parseInt(n7)
+console.log(isNaN(n8))
+let c = Number(5)
+console.log(c)
+
+
+
+let age = 45 // declaration is done only once but we can upsate any number of times
+ age = 56
+ age = 67
+
+const kk = 23 // const  variable value should be given only once.if we try to update the value it throws error.
+
+
+
+
+
+
+
---
+++ array.js
@@ -0,0 +1,57 @@
+let cars = ["volvo","hyndai","tata"]
+console.log(cars.length)
+console.log(cars.sort())
+let items = [] // empty array
+items[0] = "ball"
+items[1] = "pen"
+items[3] = 45
+items[4] = {
+    name: "arun",
+    age: 34
+}
+
+items.unshift("book")
+console.log(items)
+// let ar = new Array("volvo","hyndai","tata")
+console.log(cars[2])
+// update the value
+
+cars[1]="BMW"
+console.log(cars)
+console.log(typeof cars)
+
+// How iterate over a array:
+for(let i =0;i<cars.length;i++) {
+    console.log(cars[i])
+}
+// by using forEach method
+cars.forEach(a=>console.log(a))
+
+// to add new value to existing array
+
+cars.push("Range Rover")
+console.log(cars)
+
+// some important methods
+// to
+console.log(cars.at(2))
+console.log(cars[2])
+// volvo+hyndai+tata
+console.log(cars.join("+"))
+
+console.log(cars.pop())
+console.log(cars.shift())
+console.log(cars.concat(items))
+
+
+
+
+
+
+
+
+
+
+
+
+
